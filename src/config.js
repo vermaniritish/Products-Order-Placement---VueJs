bf696d66a571db631ccf6610925f1b7c28270562
vermaniritish <128682623+vermaniritish@users.noreply.github.com>
2026-02-09 12:43:27 +0530

    congif

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 export default {
-    base_url: 'https://app.shaguna.in',
-    api_url: "https://app.shaguna.in/api",
+    base_url: 'http://159.65.149.179',
+    api_url: "http://159.65.149.179/api",
     // base_url: 'http://127.0.0.1:8000',
     // api_url: "http://127.0.0.1:8000/api",
-    web_url: "https://shaguna.in",
+    web_url: "http://159.65.149.179",
     minOrder: 599
     
 }
